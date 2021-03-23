26f361bfaf3807d05e6582294ac136bac6016b30
iamaida <aida.mina@correounivalle.edu.co>
2021-03-23 13:48:10 -0500

    Load Chapter6

---
+++ MotorJuego/Capitulo1/2.6.ParameterizedFragmentShader/public_html/src/GLSLShaders/SimpleFS.glsl
@@ -0,0 +1,11 @@
+
+precision mediump float; 
+    // sets the precision for floating point computation
+
+// Color of pixel
+uniform vec4 uPixelColor;  
+
+void main(void) {
+    // for every pixel called sets to the user specified color
+    gl_FragColor = uPixelColor;
+}
